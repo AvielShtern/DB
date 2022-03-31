ce28dcb4f7ca02baa49e34de70395fe8d2533267
Aviel Shtern <aviel05484@gmail.com>
2022-03-31 18:12:23 +0300

    add q3

--- ex2/q4.sql
+++ ex2/q4.sql
@@ -2,7 +2,7 @@ select name, year
 from authors natural join conferences
 where institution = 'Hebrew University of Jerusalem' and subarea = 'vision'
 intersect
-select name, yesr
+select name, year
 from authors natural join conferences
 where institution = 'Hebrew University of Jerusalem' and subarea = 'ai'
 order by name, year;
