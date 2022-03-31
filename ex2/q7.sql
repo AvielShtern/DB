baf24bcb9386537acfc8b17749e0bc23549b7a89
Aviel Shtern <aviel05484@gmail.com>
2022-03-31 19:24:17 +0300

    add q7

--- ex2/q7.sql
+++ ex2/q7.sql
@@ -1,11 +1,14 @@
-select name
+select year, name
 from authors
 except
-select name
+select year, name
 from authors A1, authors A2
-where A1.institution = 'Hebrew University of Jerusalem' and A2.institution = 'Hebrew University of Jerusalem'
-      A1.conference = 'focs' and A2.conference = 'focs'
+where A1.institution = 'Hebrew University of Jerusalem' and
+      A2.institution = 'Hebrew University of Jerusalem'
+      A1.conference = 'focs'
+      and A2.conference = 'focs'
       (A1.year >= 2000 and A1.year <= 2020) and
       A1.name != A2.name and
       A1.count < A2.count and
-      A1.year = A1.year
+      A1.year = A2.year
+order by year, name;
