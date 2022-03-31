8daad4985067d6974ce13dc0ec3e1c595c4ad2f2
Aviel Shtern <aviel05484@gmail.com>
2022-03-31 19:22:18 +0300

    add q6

---
+++ ex2/q7.sql
@@ -0,0 +1,11 @@
+select name
+from authors
+except
+select name
+from authors A1, authors A2
+where A1.institution = 'Hebrew University of Jerusalem' and A2.institution = 'Hebrew University of Jerusalem'
+      A1.conference = 'focs' and A2.conference = 'focs'
+      (A1.year >= 2000 and A1.year <= 2020) and
+      A1.name != A2.name and
+      A1.count < A2.count and
+      A1.year = A1.year
