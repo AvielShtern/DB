86ab111c0ca442aa9a5da8089f02fa4ba2e3ca89
Aviel Shtern <aviel05484@gmail.com>
2022-03-31 18:55:49 +0300

    add q6

---
+++ ex2/q6.sql
@@ -0,0 +1,9 @@
+select distinct A1.name
+from authors A1
+where not exists (select conference
+                  from authors natural join conferences
+                  where name = 'Noam Nisan' and area = 'ai'
+                  except A2.conference
+                  from authors A2
+                  where A1.name = A2.name)
+order by name;
