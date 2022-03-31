73035e3b05e315abf7dcc1370c3ceef5b87c4b00
gabriel kalaora <gabriel.kalaora@sap.com>
2022-03-31 18:52:20 +0300

    apply q6.sql

---
+++ ex2/q6.sql
@@ -0,0 +1,9 @@
+select A1.name
+from authors A1
+where not exists (select conference
+                  from authors natural join conferences
+                  where name = 'Noam Nisan' and area = 'ai'
+                  except
+                  select A2.conference
+                  from authors A2
+                  where A1.name = A2.name);
