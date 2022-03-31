24c43fb0970754c498eb122337d0aa08bcbbd66f
gabriel kalaora <gabriel.kalaora@sap.com>
2022-03-31 18:27:26 +0300

    apply q5.sql

--- ex2/q5.sql
+++ ex2/q5.sql
@@ -3,5 +3,5 @@ from authors natural join conferences
 except
 select name
 from authors natural join conferences
-where area != 'system' or year >= 1990
+where area != 'systems' or year >= 1990
 order by name;
