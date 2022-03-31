8fa1a941502f60c3174746a7a1f9ed42bdd942f1
Aviel Shtern <aviel05484@gmail.com>
2022-03-31 17:52:25 +0300

    add q3

--- ex2/q3.sql
+++ ex2/q3.sql
@@ -1,4 +1,4 @@
 select distinct institution, name
 from institutions natural join authors natural join conferences
 where (area = 'ai' or subarea = 'db') and adjustedcount >= 2
-order by institutions, name;
+order by institution, name;
