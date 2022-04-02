ae7f975836bfd11b8e61fa666a5c3c3563f6ab85
gabriel kalaora <gabriel.kalaora@sap.com>
2022-04-02 20:46:58 +0300

    added README and modified q3 and q4

--- ex2/q3.sql
+++ ex2/q3.sql
@@ -1,4 +1,4 @@
 select distinct institution, name
 from institutions natural join authors natural join conferences
-where (area = 'ai' or subarea = 'db') and adjustedcount >= 2
+where (area = 'ai' or subarea = 'db') and adjustedcount >= 2 and country = 'il'
 order by institution, name;
