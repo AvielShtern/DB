fcdf512860a2851bb2599854f27ee8cf5f966e3d
Aviel Shtern <aviel05484@gmail.com>
2022-04-02 20:08:42 +0300

    fix q4

--- ex2/q4.sql
+++ ex2/q4.sql
@@ -4,5 +4,5 @@ where institution = 'Hebrew University of Jerusalem' and subarea = 'vision'
 intersect
 select name, year
 from authors natural join conferences
-where institution = 'Hebrew University of Jerusalem' and subarea = 'ai'
+where institution = 'Hebrew University of Jerusalem' and subarea = 'ml'
 order by name, year;
