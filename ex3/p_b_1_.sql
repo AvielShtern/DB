95c6880ae8dd7da665e4c75f3c1d19a1497dc51a
Aviel Shtern <aviel05484@gmail.com>
2022-04-26 18:26:30 +0300

    q3

---
+++ ex3/p_b_1_.sql
@@ -0,0 +1,4 @@
+explain analyse
+select distinct year
+from authors
+where institution = 'Hebrew University of Jerusalem'
--- ex3/q4.sql
+++ ex3/q4.sql
@@ -2,6 +2,7 @@ with count_per_country_and_region(region, country, totalCount) as
 (select region, country, sum(count) as totalCount
 from authors natural join institutions
 group by region, country)
+
 select region, country, totalCount
 from count_per_country_and_region A
 where totalCount = (select max(totalCount)
