37bb2616c29af2b5f11e92ffc7388ecb9a7f5258
gabriel kalaora <gabriel.kalaora@sap.com>
2022-05-12 15:59:46 +0300

    ex4 files

---
+++ ex4/e.sql
@@ -0,0 +1,8 @@
+explain
+select distinct *
+from authors a1
+where adjustedcount = (
+                        select max(adjustedcount)
+                        from authors a2
+                        where a2.year = a1.year
+                        );
