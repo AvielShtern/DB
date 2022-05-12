f9afe84d96de428e7c4b806a290554248369d0be
Aviel Shtern <aviel05484@gmail.com>
2022-05-12 16:53:40 +0300

    q3

--- ex3/p_b_1_.sql
+++ ex3/p_b_1_.sql
@@ -1,4 +1,6 @@
 explain analyse
-select distinct year
-from authors
-where institution = 'Hebrew University of Jerusalem'
+select distinct name, institution, conference, count, adjustedcount, year
+from authors A, (select distinct year as max_Y, max(adjustedcount) as max_A
+                 from authors
+                 group by year) T
+where A.year = T.max_Y and A.adjustedcount = T.max_A
