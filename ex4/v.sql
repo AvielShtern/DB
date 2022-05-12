163d74d92858ba219d32d4e30e7dd86bc5ab04a0
gabriel kalaora <gabriel.kalaora@sap.com>
2022-05-12 16:55:19 +0300

    ex4 files

--- ex4/v.sql
+++ ex4/v.sql
@@ -1,4 +1,6 @@
-create view table_year as
-    select distinct year,  max(adjustedcount)
-    from authores
-    group by year;
+explain analyse
+select distinct name, institution, conference, count, adjustedcount, year
+from authors A, (select distinct year as max_Y, max(adjustedcount) as max_A
+                 from authors
+                 group by year) T
+where A.year = T.max_Y and A.adjustedcount = T.max_A
