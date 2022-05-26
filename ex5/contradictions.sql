a9936bab4382a1b1c02f9a6c191dd1611d4d15f3
gabriel kalaora <gabriel.kalaora@sap.com>
2022-05-26 19:41:17 +0300

    files ex5

---
+++ ex5/contradictions.sql
@@ -0,0 +1,9 @@
+create table sales(
+    TransactionNo varchar,
+    Date varchar,
+    ProductNo varchar,
+    ProductName varchar,
+    Price varchar,
+    CustomerNo varchar,
+    Country varchar
+)
