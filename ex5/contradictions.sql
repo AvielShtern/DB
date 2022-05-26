866436ad19e6395e64e8ab82f53d787e07af967d
gabriel kalaora <gabriel.kalaora@sap.com>
2022-05-26 20:39:25 +0300

    files ex5

--- ex5/contradictions.sql
+++ ex5/contradictions.sql
@@ -4,4 +4,4 @@ where (T.TransactionNo = S.TransactionNo and T.date != S.date) or
       (T.TransactionNo = S.TransactionNo and T.CustomerNo != S.CustomerNo) or
       (T.ProductNo = S.ProductNo and T.ProductName != S.ProductName) or
       (T.CustomerNo = S.CustomerNo and T.country != S.country)
-order by TransactionNo ProductNo
+order by TransactionNo, ProductNo
